2f8b17195e3d1892f8306caac8c8d99372b5cf03
Jahid Hasan <jsajeeb09@gmail.com>
2019-02-09 19:05:12 +0600

    F-04: Implement Log in Module

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Dashboard } from './components/dashboard/Dashboard';
 import { LandingPage } from './components/landingPage/LandingPage';
+import {LoginPage} from './components/login/LoginPage';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <Switch>
         <Route exact path='/' component={LandingPage}/>
         <Route path='/dashboard' component={Dashboard}/>
+        <Route path='/login' component={LoginPage}/>
       </Switch>
     );
   }
