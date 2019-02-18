c5f4d02379e3463d46689717b190384b159cdd1b
Jahid Hasan <jsajeeb09@gmail.com>
2019-02-19 02:04:43 +0600

    F-14: Add Routing and Update Design

--- clientapp/src/components/home/Home.jsx
+++ clientapp/src/components/home/Home.jsx
@@ -1,69 +1,87 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { HashRouter, Route, NavLink } from "react-router-dom";
 
 import Styles from "./Home.less";
 import Products from "./products/Products.jsx";
 import Customers from "./customers/Customers.jsx";
+import Dashboard from "./dashboard/Dashboard.jsx";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        {/* sidebar content */}
-        <div className={Styles.sidebar}>
-          <div className={Styles.sidebarheader}>
-            <div className={Styles.headertext + " " + Styles.sidebarheadertext}>
-              BASE
+      <HashRouter>
+        <div>
+          {/* sidebar content */}
+          <div className={Styles.sidebar}>
+            <div className={Styles.sidebarheader}>
+              <div
+                className={Styles.headertext + " " + Styles.sidebarheadertext}
+              >
+                BASE
+              </div>
             </div>
+            <div className={Styles.userpanel}>
+              <div id="name">Md Muidul Alam</div>
+              <div>
+                <i className="fa fa-circle text-success" /> Online
+              </div>
+            </div>
+            <ul className={Styles.menu}>
+              <li className={Styles.menubar}>MAIN NAVIGATION</li>
+              <li className={Styles.menulineitem}>
+                <NavLink to="/" className={Styles.menunavlink}>
+                  <i className="fas fa-home" /> Home
+                </NavLink>
+              </li>
+              <li className={Styles.menulineitem}>
+                <i className="fas fa-shopping-cart" /> Sales
+              </li>
+              <li className={Styles.menulineitem}>
+                <NavLink to="/customers" className={Styles.menunavlink}>
+                  <i className="fas fa-snowman" /> Customers
+                </NavLink>
+              </li>
+              <li className={Styles.menulineitem}>
+                <NavLink to="/products" className={Styles.menunavlink}>
+                  <i className="fas fa-database" /> Products
+                </NavLink>
+              </li>
+              <li className={Styles.menulineitem}>
+                <i className="fas fa-file-invoice-dollar" /> Purchases
+              </li>
+              <li className={Styles.menulineitem}>
+                <i className="fas fa-user-tie" /> Vendors
+              </li>
+            </ul>
           </div>
-          <div className={Styles.userpanel}>
-            <div id="name">Md Muidul Alam</div>
+          {/* main content */}
+          <div className={Styles.maincontent}>
+            <div className={Styles.header + " row"}>
+              <div className="col-lg-1 col-md-1 col-sm-1">
+                <div className={Styles.menuminimize}>
+                  <i className="fas fa-bars" />
+                </div>
+              </div>
+              <div className="col-lg-8 col-md-6 col-sm-5">
+                <div className={Styles.headertext}>BASE COMPANY LTD.</div>
+              </div>
+              <div className="col-lg-3 col-md-5 col-sm-4">
+                <div className={Styles.searchbox}>
+                  <input
+                    className={Styles.searchboxinput + " form-control"}
+                    placeholder="Search"
+                  />
+                </div>
+              </div>
+            </div>
             <div>
-              <i className="fa fa-circle text-success" /> Online
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/customers" component={Customers} />
+              <Route path="/products" component={Products} />
             </div>
           </div>
-          <ul className={Styles.menu}>
-            <li className={Styles.menubar}>MAIN NAVIGATION</li>
-            <li className={Styles.menulineitem}>
-              <i className="fas fa-shopping-cart" /> Sales
-            </li>
-            <li className={Styles.menulineitem}>
-              <i className="fas fa-snowman" /> Customers
-            </li>
-            <li className={Styles.menulineitem}>
-              <i className="fas fa-database" /> Products
-            </li>
-            <li className={Styles.menulineitem}>
-              <i className="fas fa-file-invoice-dollar" /> Purchases
-            </li>
-            <li className={Styles.menulineitem}>
-              <i className="fas fa-user-tie" /> Vendors
-            </li>
-          </ul>
         </div>
-        {/* main content */}
-        <div className={Styles.maincontent}>
-          <div className={Styles.header + " row"}>
-            <div className="col-lg-1 col-md-1 col-sm-1">
-              <div className={Styles.menuminimize}>
-                <i className="fas fa-bars" />
-              </div>
-            </div>
-            <div className="col-lg-8 col-md-6 col-sm-5">
-              <div className={Styles.headertext}>BASE COMPANY LTD.</div>
-            </div>
-            <div className="col-lg-3 col-md-5 col-sm-4">
-              <div className={Styles.searchbox}>
-                <input
-                  className={Styles.searchboxinput + " form-control"}
-                  placeholder="Search"
-                />
-              </div>
-            </div>
-          </div>
-          <div>Pages Here</div>
-        </div>
-      </div>
+      </HashRouter>
     );
   }
 }
