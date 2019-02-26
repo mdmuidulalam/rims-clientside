a4124126b5ceed194308e6302d274c774962aea5
Jahid Hasan <jsajeeb09@gmail.com>
2019-02-27 00:13:31 +0600

    F-23: Added products grid system

--- clientapp/src/components/home/products/productsGrid/ProductsGrid.jsx
+++ clientapp/src/components/home/products/productsGrid/ProductsGrid.jsx
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
+import axios from "axios";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import GridStyles from "../../../../utilities/css/Grid.less";
 
 class ProductsGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  getProducts = () => {
+    axios
+      .get("/api/products/get")
+      .then(response => {
+        if (response.data.success) {
+          this.setState({ products: response.data.entity });
+        } else {
+          throw "something went wrong";
+        }
+      })
+      .catch(error => {});
+  };
+
+  //should be moved to utilities/reactTable.js
+  filterMethod = (filter, row, column) => {
+    const id = filter.pivotId || filter.id;
+    return row[id] !== undefined
+      ? String(row[id].toLowerCase()).startsWith(filter.value.toLowerCase())
+      : true;
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
   render() {
-    return <div>ProductsGrid</div>;
+    const columns = [
+      {
+        Header: "ID",
+        accessor: "id"
+      },
+      {
+        Header: "Product Name",
+        accessor: "ProductName"
+      },
+      {
+        Header: "Product Price",
+        accessor: "ProductPrice"
+      },
+      {
+        Header: "Quantity",
+        accessor: "QuantityOnHand"
+      }
+    ];
+
+    return (
+      <div>
+        <div className={GridStyles.gridTitle}>
+          <h1>Products</h1>
+        </div>
+        <div className={GridStyles.gridBody}>
+          <ReactTable
+            data={this.state.products}
+            columns={columns}
+            filterable
+            defaultFilterMethod={this.filterMethod}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
