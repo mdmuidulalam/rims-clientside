6f15e0f82463893f3d73060db0ba4ba673b2c8f2
Jahid Hasan <jsajeeb09@gmail.com>
2019-03-27 22:10:37 +0600

    F-30: Fixed accounting module

--- clientapp/src/services/Accounting.js
+++ clientapp/src/services/Accounting.js
@@ -1,24 +1,29 @@
 import axios from "axios";
-import * as Promise from "bluebird";
+import Promise from "bluebird";
 
 export default class AccountingService {
   getAccountingData() {
     return new Promise((resolve, reject) => {
       //return axios.get("api/..");
-      resolve([
-        {
-          ID: 1,
-          Type: "Expense",
-          Amount: 500,
-          Description: "Internet Bill"
-        },
-        {
-          ID: 1,
-          Type: "Income",
-          Amount: 5000,
-          Description: "Robbery"
+      resolve({
+        data: {
+          success: true,
+          entity: [
+            {
+              Id: 1,
+              AccountingType: "Expense",
+              Amount: 500,
+              Description: "Internet Bill"
+            },
+            {
+              Id: 1,
+              AccountingType: "Income",
+              Amount: 5000,
+              Description: "Robbery"
+            }
+          ]
         }
-      ]);
+      });
     });
   }
 }
